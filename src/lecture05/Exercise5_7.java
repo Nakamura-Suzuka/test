d7db94fdec22ad4f4497ea4d75cfef37b6b1627d
Nakamura-Suzuka <Idia3939Malleus@extinctionmagic@ezweb.ne.jp>
2021-07-07 21:41:49 +0900

    lecture05を完了

--- src/lecture05/Exercise5_7.java
+++ src/lecture05/Exercise5_7.java
@@ -3,12 +3,18 @@ package lecture05;
 import java.util.ArrayList;
 
 public class Exercise5_7 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         //4つのインスタンスを末尾に追加する
-        ArrayList<Insect> List = new ArrayList<>();
-        List.add(Insect insect);
-        List.add(Butterfly butterfly);
-        List.add(Locust locust);
-        List.add(SwallowtailButterfly swallowtailButterfly);
+        ArrayList<Insect> list = new ArrayList<>();
+        list.add(new Insect()); //オブジェクトの生成
+        list.add(new Butterfly());
+        list.add(new Locust());
+        list.add(new SwallowtailButterfly());
+
+        //拡張for文
+        for(Insect insects : list){
+            insects.move();
+        }
+        //基本大文字はクラス名、小文字は変数名
     }
 }
